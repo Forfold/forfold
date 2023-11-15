3696cc02f4a262df3d601f67c4c37a0861eb7afc
Forfold <nathanieljcook@outlook.com>
2023-11-14 22:48:11 -0800

    fix track reference

--- src/SoundCloudFrames.tsx
+++ src/SoundCloudFrames.tsx
@@ -41,7 +41,7 @@ export default function SoundCloudFrames() {
           scrolling="no"
           frameBorder="no"
           allow="autoplay"
-          src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/11199910012&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
+          src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1119997012&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
         />
       </Grid>
     </Grid>
