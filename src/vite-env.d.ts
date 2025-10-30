ef5b1bdd69f5e33bb654a1bb3673e0c871c7de3c
Forfold <nathanieljcook@outlook.com>
2025-10-30 15:51:02 -0700

    remove JPG module

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -14,11 +14,3 @@ declare module '*.png' {
   const src: string
   export default src
 }
-
-// if your file is literally named avatar.JPG (uppercase)
-// TS is case-sensitive about module patterns on some setups,
-// so include this too:
-declare module '*.JPG' {
-  const src: string
-  export default src
-}
