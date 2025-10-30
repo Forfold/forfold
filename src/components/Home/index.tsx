2b9236b6014bd46bf93bdf0da586b0e54a6bf7ed
Forfold <nathanieljcook@outlook.com>
2025-10-30 00:32:21 -0700

    update layout

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,51 +1,65 @@
-import { Box, Typography } from '@mui/material'
+import { Box, SxProps, Typography } from '@mui/material'
 import { ProfilePhoto } from '../ProfilePhoto'
+import React from 'react'
 
 export default function Home() {
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        alignItems: 'flex-start',
-        gap: 4,
-      }}
-    >
-      {/* left: avatar + basic info */}
+    <React.Fragment>
       <Box
         sx={{
-          minWidth: 120,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          textAlign: 'center',
-          gap: 2,
+          display: { xs: 'none', md: 'flex' },
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+          alignItems: 'flex-start',
+          gap: 4,
         }}
       >
-        <ProfilePhoto />
+        <Box
+          sx={{
+            minWidth: 120,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            textAlign: 'center',
+            gap: 2,
+          }}
+        >
+          <ProfilePhoto />
+        </Box>
+
+        <HomeText />
       </Box>
 
-      {/* right: main text */}
-      <Box sx={{ flex: 1 }}>
-        <Typography variant="h4" component="h2" sx={{ pb: 2 }}>
-          Nathaniel Cook
-        </Typography>
-        <Typography variant="h6" component="h3" sx={{ pb: 2 }}>
-          I design, build, and maintain complex front-end systems and APIs — and I produce and edit
-          audio.
-        </Typography>
-
-        <Typography sx={{ pb: 1 }}>
-          I&apos;m a senior software engineer focused on delivering a great user experience. I
-          design and ship tools that help teams see what their systems are doing, in production, in
-          real time.
-        </Typography>
-        <Typography>
-          I also have a certificate in Audio Engineering Principals. I focus on drum recordings,
-          mixing, vocal narration, and voice acting. Contact me for more information!
-        </Typography>
+      <Box sx={{ display: { xs: 'block', md: 'none' } }}>
+        <Box sx={{ display: 'flex', justifyContent: 'center', pb: 2 }}>
+          <ProfilePhoto />
+        </Box>
+        <HomeText styleProps={{ textAlign: 'center' }} />
       </Box>
+    </React.Fragment>
+  )
+}
+
+function HomeText({ styleProps }: { styleProps?: SxProps }) {
+  return (
+    <Box sx={{ flex: 1 }}>
+      <Typography variant="h4" component="h2" sx={{ pb: 2, ...styleProps }}>
+        Nathaniel Cook
+      </Typography>
+      <Typography variant="h6" component="h3" sx={{ pb: 2 }}>
+        I design, build, and maintain complex front-end systems and APIs — and I produce and edit
+        audio.
+      </Typography>
+
+      <Typography sx={{ pb: 1 }}>
+        I&apos;m a senior software engineer focused on delivering a great user experience. I design
+        and ship tools that help teams see what their systems are doing, in production, in real
+        time.
+      </Typography>
+      <Typography>
+        I also have a certificate in Audio Engineering Principals. I focus on drum recordings,
+        mixing, vocal narration, and voice acting. Contact me for more information!
+      </Typography>
     </Box>
   )
 }
